ab0b6784356c16dc1ece2037e01ad8ef957ec75e
Deepak126-126 <72086546+Deepak126-126@users.noreply.github.com>
2020-10-29 18:39:57 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,19 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      // this is the position of the object
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+      
+    }
+  };
---
+++ README.md
@@ -0,0 +1,3 @@
+# angryBirdsStage2.5
+Angry Birds stage 2.5 with Class Inheritance and Images
+Hello all my name is deepak and i am a proud yellow hat coder in whitehat jr
